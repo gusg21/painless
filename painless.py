c7d16ebce2f24224c49a6afaaece96638cf491c0
gusg21 <gusg21@parkschool.net>
2017-05-01 18:24:24 -0400

    here comes the versioning...

--- painless.py
+++ painless.py
@@ -1,15 +1,18 @@
 import argparse, sys, yaml
 import generate, serve
 
-'''
+VERSION = "v0.0"
+
+__doc__ = '''
 A simple markdown based static site generator written in Python. Meant to make site creation painless, hence the name.
-'''
+
+Version: ''' + VERSION
 
 def main():
     # Argument parser
     parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument("command", help="Command for painless (gen, serve)")
+    parser.add_argument("command", help="Command for painless (gen, serve, version)")
 
     args = parser.parse_args(sys.argv[1:])
 
@@ -20,9 +23,11 @@ def main():
         generate.generate(config["options"]["outputDirectory"])
     elif args.command.lower() == "serve":
         serve.serve(PORT=config["options"]["port"], SERVE=config["options"]["outputDirectory"])
+    elif args.command.lower() == "version":
+        print("Version: " + VERSION)
     else:
         print("Unknown command: " + args.command)
-        print("Command is one of: gen, serve")
+        print("Command is one of: gen, serve, version")
 
 if __name__ == '__main__':
     main()
